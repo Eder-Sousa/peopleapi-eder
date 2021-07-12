a9bc94a915895a8832ec13f227fc2e9e8643dd34
Eder-Sousa <ederdi395@gmail.com>
2021-07-12 12:32:14 -0300

    Created README.md file

--- src/main/java/com/edersousa/personapi/controller/PersonController.java
+++ src/main/java/com/edersousa/personapi/controller/PersonController.java
@@ -5,6 +5,7 @@ import com.edersousa.personapi.dto.responose.MessageResponseDTO;
 import com.edersousa.personapi.exception.PersonNotFoundException;
 import com.edersousa.personapi.service.PersonService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 

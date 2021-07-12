b56b251cd4cd72fa9c2b5c8920e76a6fa507d4af
Eder-Sousa <ederdi395@gmail.com>
2021-07-12 20:07:29 -0300

    Updated README.md file

--- src/main/java/com/edersousa/personapi/controller/PersonController.java
+++ src/main/java/com/edersousa/personapi/controller/PersonController.java
@@ -13,7 +13,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/peoples")
+@RequestMapping("/api/v1/people")
 @AllArgsConstructor
 public class PersonController {
 
